f32478a1acac43b416324a2b567e8c73c9003f17
Rudraksh Gupta <rudrakshgupta09@gmail.com>
2024-06-22 14:19:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,25 +11,25 @@ with st.sidebar:
     st.write('Created by Rudraksh Gupta and Ayush Bhowal.')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        replicate_api = st.secrets['REGISTRATION_TOKEN']
     else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        replicate_api = st.text_input('Enter your token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+            st.warning('Please enter your registration token', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    os.environ['REGISTRATION_TOKEN'] = replicate_api
 
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
+    st.subheader('Models/Verison')
+    selected_model = st.sidebar.selectbox('Choose a Asclepius Model/Version', ['Asclepius-1.0', 'Asclepius-2.0'], key='selected_model')
+    if selected_model == 'Asclepius-1.0':
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
+    elif selected_model == 'Asclepius-2.0':
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    st.markdown('Meet our creator [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
